7a4b3693450ff456a3cd727f39bc415d59dd0b1a
stivenlovera <stivenlovera@gmail.com>
2022-06-05 16:57:43 -0400

    add angular universal y pwa

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ContactosComponent } from './components/contactos/contactos.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'contacts',
+    component:ContactosComponent,
+    pathMatch: 'full'
+  }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {
+    initialNavigation: 'enabledBlocking'
+})],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/components/contactos/contactos.component.ts
+++ src/app/components/contactos/contactos.component.ts
@@ -1,18 +1,17 @@
 import { Component, OnInit } from '@angular/core';
-import { Meta, Title } from '@angular/platform-browser';
-
+import { Title, Meta } from '@angular/platform-browser';
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: 'app-contactos',
+  templateUrl: './contactos.component.html',
+  styleUrls: ['./contactos.component.css']
 })
-export class AppComponent implements OnInit {
+export class ContactosComponent implements OnInit {
+
   constructor(
     private metaTagService: Meta,
     private titleService:Title
-  ){
+  ) { }
 
-  }
   ngOnInit(): void {
     this.titleService.setTitle("Esto son contactos");
     this.metaTagService.addTags([
@@ -25,13 +24,13 @@ export class AppComponent implements OnInit {
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { name: 'date', content: '2019-10-31', scheme: 'YYYY-MM-DD' },
       { charset: 'UTF-8' },
-      { name: 'og:type', content: 'annix' },
+      { name: 'og:type', content: 'annix-contactos' },
       { name: 'og:url', content: 'https://anniix.com' },
-      { name: 'og:title', content: 'Test inicio' },
-      { name: 'og:description', content: 'Descripcion de inicio' },
-      { name: 'og:image', content: 'https://ik.imagekit.io/u1gy0bysycr/images_3MRGTjOVc.jfif?ik-sdk-version=javascript-1.4.3&updatedAt=1651940965024' },
+      { name: 'og:title', content: 'Test contacts' },
+      { name: 'og:description', content: 'Descripcion de contactos' },
+      { name: 'og:image', content: 'https://ik.imagekit.io/u1gy0bysycr/327274-love-live-school-idol-paradise-vol-3-lily-white-ps-vita-front-cover_dh8HDfjoK.jpg?ik-sdk-version=javascript-1.4.3&updatedAt=1651940910898' },
       //metas de  prueba
     ]);
   }
-  title = 'anniix-ssr';
+
 }
